4b318fd76f335d036f03e44713b63cef06896d6a
barckcode <barckcode@gmail.com>
2020-08-08 13:56:25 +0200

    Add [3 - 6] options - gestion_usuarios.sh

--- gestion_usuarios.sh
+++ gestion_usuarios.sh
@@ -7,7 +7,7 @@ USER=""
 
 #Menu de opciones:
     echo "_____________________________________________________"
-    echo "             Programa de Gestion de Usurios          "
+    echo "          Programa de Gestion de Usurios             "
     echo "_____________________________________________________"
     echo "                 MENU PRINCIPAL                      "
     echo "_____________________________________________________"
@@ -16,11 +16,11 @@ USER=""
     echo "3. Eliminar Usuarios."
     echo "4. Añadir Usuarios a un Grupo."
     echo "5. Eliminar Usuarios de un Grupo."
-    echo "7. Cambiar Password del Usuario."
-    echo "8. Verificar si el Usuario Esta Caducado o Bloqueado."
-    echo "9. Desbloquear Usuario."
-    echo "10. Dar permisos de escalado a root a un Usuario"
-    echo "11. Dar permisos de sudo a un Usuario"
+    echo "6. Cambiar Password del Usuario."
+    echo "7. Verificar si el Usuario Esta Caducado o Bloqueado."
+    echo "8. Desbloquear Usuario."
+    echo "9. Dar permisos de escalado a root a un Usuario"
+    echo "10. Dar permisos de sudo a un Usuario"
     echo "_____________________________________________________"
 
 #Leer opcion elegida por el usuario:
@@ -35,7 +35,7 @@ case $OPTIONS in
         echo "Has elegio la opcion $OPTIONS"
         echo "."
         read -p "Usuario a comprobar: " USER
-        
+
         echo "_____________________________________________________"
         echo "              SALIDA DE LOS COMANDOS                 "
         id $USER
@@ -58,7 +58,7 @@ case $OPTIONS in
         echo "."
         read -p "Usuario a crear: " USER
         read -p "Nombre o Actividad del Usuario: " MESSAGE
-        read -p "¿Quieres añadirlo a algun grupo? [S/n]: " OPTION 
+        read -p "¿Quieres añadirlo a algun grupo? [S/n]: " OPTION
 
         if [[ "$OPTION" = "S" || "$OPTION" = "s" ]]
         then
@@ -98,37 +98,129 @@ case $OPTIONS in
         echo "."
         echo "*Necesitas permisos para ejecutar esta tarea*"
         echo "."
-        
+        read -p "Usuario a eliminar: " USER
+
+        echo "_____________________________________________________"
+        echo "              SALIDA DE LOS COMANDOS                 "
+        userdel -f -r $USER
+        VALIDATION=`echo $?`
+
+        echo "_____________________________________________________"
+        echo "                      RESULTADO                      "
+        if [[ VALIDATION -eq 0 ]]
+        then
+            echo "El usuario $USER se ha eliminado correctamente."
+        else
+            echo "El usuario $USER NO se ha podido eliminar."
+        fi
         ;;
     4)
         echo "."
         echo "Has elegio la opcion $OPTIONS"
         echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
+        read -p "Usuario a añadir en un grupo: " USER
+        read -p "Grupo al que añadir al usuario: " GROUP
+
+        echo "_____________________________________________________"
+        echo "              SALIDA DE LOS COMANDOS                 "
+        usermod -a -G $GROUP $USER
+        id $USER
+        VALIDATION=`echo $?`
+
+        echo "_____________________________________________________"
+        echo "                      RESULTADO                      "
+        if [[ VALIDATION -eq 0 ]]
+        then
+            echo "El usuario $USER se ha añadido correctamente a $GROUP."
+        else
+            echo "El usuario $USER NO se ha podido añadir a $GROUP."
+        fi
         ;;
     5)
         echo "."
         echo "Has elegio la opcion $OPTIONS"
         echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
+        read -p "Usuario a eliminar del grupo: " USER
+        read -p "Grupo del que quitar al usuario: " GROUP
+
+        echo "_____________________________________________________"
+        echo "              SALIDA DE LOS COMANDOS                 "
+        deluser $USER $GROUP
+        id $USER
+        VALIDATION=`echo $?`
+
+        echo "_____________________________________________________"
+        echo "                      RESULTADO                      "
+        if [[ VALIDATION -eq 0 ]]
+        then
+            echo "El usuario $USER se ha retirado correctamente del grupo $GROUP."
+        else
+            echo "El usuario $USER NO se ha podido retirar del grupo $GROUP."
+        fi
         ;;
     6)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
+        read -p "Usuario a cambiar la contraseña: " USER
+        read -p "Nueva constraseña: " PASSWORD
+
+        echo "_____________________________________________________"
+        echo "              SALIDA DE LOS COMANDOS                 "
+        echo -e "$PASSWORD\n$PASSWORD" | passwd $USER
+        VALIDATION=`echo $?`
+
+        echo "_____________________________________________________"
+        echo "                      RESULTADO                      "
+        if [[ VALIDATION -eq 0 ]]
+        then
+            echo "Se ha modificado la contraseña del usuario $USER correctamente"
+        else
+            echo "NO se ha podido modificar la contraseña del usuario $USER"
+        fi
         ;;
     7)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
         ;;
     8)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
         ;;
     9)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
         ;;
     10)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
         ;;
     11)
+        echo "."
         echo "Has elegio la opcion $OPTIONS"
-        ;;  
-    *) 
+        echo "."
+        echo "*Necesitas permisos para ejecutar esta tarea*"
+        echo "."
+        ;;
+    *)
         echo "ERROR: No has elegido ninguna opcion disponible."
         ;;
 esac
